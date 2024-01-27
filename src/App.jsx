82b3ceb098915138b5e253826eb0236ab6e8dc06
Dond1D <donineti@hotmail.com>
2024-01-27 03:43:37 +0100

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   import React from "react";
   import ReactDOM from "react-dom";
   import { Route, Routes,useNavigate } from "react-router-dom";
@@ -38,22 +37,5 @@
       </div>
     )
   }
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-// import Router from "react-router-dom"
-
-export default function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
-  )
-}
->>>>>>> 80359f5570d845048ef5852cbc2484606090cb71
 
 
